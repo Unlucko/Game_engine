bccedb3508ce55554d25ad15b6c968df1f974120
Unlucko <cesarfsegurap.09@gmail.com>
2024-04-17 20:37:50 -0500

    Primeras ideas de lo básico

---
+++ src/main.cpp
@@ -0,0 +1,23 @@
+#include <vulkan/vulkan.h>
+#include <iostream>
+
+int main(int argc, char const *argv[])
+{
+    VkApplicationInfo appInfo = {};
+    appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+    appInfo.pApplicationName = "Pong";
+    appInfo.pEngineName = "Ponggine";
+
+    VkInstanceCreateInfo instanceInfo = {};
+    instanceInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+    instanceInfo.pApplicationInfo = &appInfo;
+
+    VkInstance instance;
+
+    VkResult result = vkCreateInstance(&instanceInfo, 0, &instance);
+    if (result == VK_SUCCESS){
+        std::cout << "succesfully create vulkan instance" << std::endl;
+    }
+
+    return 0;
+}
